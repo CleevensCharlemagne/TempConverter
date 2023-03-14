5e02a4323a825f74923be6db19f8c2d5381a4f68
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-13 21:34:22 -0400

    update

--- main.c
+++ main.c
@@ -11,4 +11,8 @@ int main()
 
      /* celsius to fahrenheit conversion formula */
     fahrenheit = (celsius * 9 / 5) + 32;
+
+     printf("%.2f Celsius = %.2f Fahrenheit", celsius, fahrenheit);
+
+    return 0;
 }
